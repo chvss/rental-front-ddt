0ce53e6c6b9bb64757abcf5d77b19458370dafe7
chvss <chvss.era@gmail.com>
2021-05-23 07:55:48 +0300

    Бронирования

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,16 +1,73 @@
 <template>
-  <v-alert
-    text
-    outlined
-    color="deep-orange"
-    icon="mdi-fire"
-  >
-    Корзина
-  </v-alert>
+  <div>
+    <page-header
+        title="Корзина"
+        subtitle="В данном разделе представленна история ваших бронирований">
+    </page-header>
+
+    <v-alert
+        outlined
+        type="warning"
+        prominent
+        border="left"
+    >
+      Вы забронировали "Лыжи" в кол-ве 2шт у "Ельская СОШ".
+
+      <div class="absolute r-0 b-0">
+        <v-btn
+            color="red lighten-2"
+            text
+        >
+          Отменить бронирование
+        </v-btn>
+      </div>
+    </v-alert>
+
+    <v-alert
+        outlined
+        type="success"
+        text
+    >
+      Вы забронировали "Скейт" в кол-ве 1шт у "Ельская СОШ".
+
+      <div class="absolute r-1 b-0">
+          21.05.2021
+      </div>
+    </v-alert>
+
+    <v-alert
+        outlined
+        type="success"
+        text
+    >
+      Вы забронировали "Велосипед" в кол-ве 1шт у "Прокат спортивного инвентаря".
+
+      <div class="absolute r-1 b-1">
+        18.05.2021
+      </div>
+    </v-alert>
+
+    <v-alert
+        outlined
+        type="success"
+        text
+    >
+      Вы забронировали "Бассейн" в кол-ве 1шт у "Водный мир".
+
+      <div class="absolute r-1 b-1">
+        11.05.2021
+      </div>
+    </v-alert>
+  </div>
 </template>
 
 <script>
+import PageHeader from '@/components/blocks/PageHeader';
+
 export default {
-    name: 'Cart'
+    name: 'Cart',
+    components: {
+        PageHeader,
+    }
 };
 </script>
